2fa8b0487917cbf16f4d75428a74389be2cfbf4b
kaldor.levente.vikto <kaldor.levente.viktor.2022i@bankitatabanya.hu>
2026-04-16 13:09:17 +0200

    feat: szimulacio(), jelentes(), legjobbanJovedelmez(), vesztesGepek(), osszProfit() metódusok implementálva

--- src/JatekGep.java
+++ src/JatekGep.java
@@ -1,2 +1,43 @@
 public class JatekGep {
+    protected int azonosito;
+    protected String nev;
+    protected double minimumTet;
+    protected double maximumTet;
+    protected double osszesBevet;
+    protected double osszesKifizetes;
+    protected int jatszottKorok;
+
+    public JatekGep(int azonosito, String nev, double minimumTet, double maximumTet) {
+        this.azonosito = azonosito;
+        this.nev = nev;
+        this.minimumTet = minimumTet;
+        this.maximumTet = maximumTet;
+        this.osszesBevet = 0.0;
+        this.osszesKifizetes = 0.0;
+        this.jatszottKorok = 0;
+    }
+
+    public boolean tetEllnorzes(double tet) {
+        if (tet > minimumTet && tet < maximumTet) {
+            return true;
+        }
+        System.out.println("Érvénytelen tét: " + tet + " [min: " + minimumTet + " – " + maximumTet + " : 100.0])");
+        return false;
+    }
+
+    public double jatszik(double tet) {
+        return 0.0;
+    }
+
+    public double getProfit() {
+        return osszesBevet - osszesKifizetes;
+    }
+
+    public String getAdatlap() {
+        return "["+ azonosito + "] " + nev + " | Tét: " + minimumTet + " – " + maximumTet + " Ft | Körök: " + jatszottKorok;
+    }
+
+    public String getStatisztika() {
+        return "Bevétel: " + osszesBevet + " Ft | Kifizetés: " + osszesKifizetes + " Ft | Profit: " + getProfit() + " Ft";
+    }
 }
--- src/Kaszino.java
+++ src/Kaszino.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Kaszino {
     private JatekGep[] gepek;
     private int darabszam;
@@ -9,9 +11,58 @@ public class Kaszino {
 
     public void gepFelvet(JatekGep g) {
         if (!(darabszam < gepek.length)) {
-            throw new IllegalStateException("A kaszinó megtelt, nem vehető fel több gép")
+            throw new IllegalStateException("A kaszinó megtelt, nem vehető fel több gép");
         }
         gepek[darabszam] = g;
         darabszam++;
     }
+
+    public void szimulacio(int korok) {
+        Random r = new Random();
+        int i = 0;
+        for (JatekGep jatekGep : gepek) {
+            i++;
+            double tet = r.nextDouble(jatekGep.minimumTet, jatekGep.maximumTet);
+            jatekGep.jatszik(tet);
+            System.out.println("Szimuláció: " + jatekGep.nev + " – " + i + ". kör");
+        }
+    }
+
+    public void jelentes() {
+        for (JatekGep jatekGep : gepek) {
+            System.out.println(jatekGep.getAdatlap());
+            System.out.println(jatekGep.getStatisztika());
+            System.out.println(osszProfit());
+        }
+    }
+
+    public JatekGep legjobbanJovedelmez() {
+        JatekGep max = gepek[0];
+        for (JatekGep jatekGep : gepek) {
+            max = (jatekGep.getProfit() > max.getProfit()) ? jatekGep : max;
+        }
+        if (max == null) {
+            System.out.println("A kaszinóban nincs egy gép sem");
+            return null;
+        }
+        return max;
+    }
+
+    public void vesztesGepek() {
+        for (JatekGep jatekGep : gepek) {
+            if (jatekGep.getProfit() < 0) {
+                System.out.println(jatekGep.getAdatlap());
+            }
+        }
+    }
+
+    public double osszProfit() {
+        double sum = 0;
+        for (JatekGep jatekGep : gepek) {
+            sum += jatekGep.getProfit();
+        }
+        return sum;
+    }
+
+
 }
