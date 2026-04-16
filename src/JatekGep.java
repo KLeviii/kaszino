57b8d041e74c644ebc5d1b6d0e93e5ad1eef9a8b
kaldor.levente.vikto <kaldor.levente.viktor.2022i@bankitatabanya.hu>
2026-04-16 12:16:01 +0200

    feat: adattagok létrehozva

---
+++ src/JatekGep.java
@@ -0,0 +1,11 @@
+public class JatekGep {
+    protected int azonosito;
+    protected String nev;
+    protected double minimumTet;
+    protected double maximumTet;
+    protected double osszesBevet;
+    protected double osszesKifizetes;
+    protected int jatszottKorok;
+
+
+}
