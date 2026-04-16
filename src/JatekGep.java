9bc859d7da94ec19c4ffa7b8eb3f25bbb0b1567a
kaldor.levente.vikto <kaldor.levente.viktor.2022i@bankitatabanya.hu>
2026-04-16 12:48:34 +0200

    feat: Kaszino osztály létrehozva, konstruktorral

---
+++ src/JatekGep.java
@@ -0,0 +1,2 @@
+public class JatekGep {
+}
---
+++ src/Kaszino.java
@@ -0,0 +1,11 @@
+public class Kaszino {
+    private JatekGep[] gepek;
+    private int darabszam;
+
+    public Kaszino(int maxGepek) {
+        this.gepek = new JatekGep[maxGepek];
+        this.darabszam = 0;
+    }
+
+
+}
